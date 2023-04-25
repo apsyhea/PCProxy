d258f605765009a200dc45d9ba0d6995867d617a
apsyhea <apsyhea@gmail.com>
2023-04-25 16:23:39 +0300

    update

---
+++ crunch.py
@@ -0,0 +1,2 @@
+python = "Python и 1000 программ"
+print(python.find('р'))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from asyncio import Task
 import asyncio
 import aiohttp
 from bs4 import BeautifulSoup, NavigableString, Tag
-from asyncio import Task
 
 # A dictionary that maps column names to their indices in the proxy table.
 COLUMNS: dict[str, int] = {
@@ -16,8 +16,10 @@ COLUMNS: dict[str, int] = {
 }
 
 # Given a list of <td> elements, extract the relevant proxy information and return it as a tuple.
+
+
 def format_proxy_info(all_td) -> tuple[str, str]:
-    # Extract the individual proxy fields from the <td> elements.
+    """ Extract the individual proxy fields from the <td> elements."""
     ip_address: str = all_td[COLUMNS['IP Address']].text
     port: str = all_td[COLUMNS['Port']].text
     code: str = all_td[COLUMNS['Code']].text
@@ -27,64 +29,78 @@ def format_proxy_info(all_td) -> tuple[str, str]:
     https: str = all_td[COLUMNS['Https']].text
     last_checked: str = all_td[COLUMNS['Last Checked']].text
     # Return the proxy information as a tuple.
-    return f"{ip_address}:{port}", "{:<20} {:<10} {:<5} {:<25} {:<15} {:<5} {:<5} {:<10}".format(
-            ip_address, port, code, country, anonymity, google, https, last_checked)
+    return (
+        f"{ip_address}:{port}",
+        f"{ip_address:<20} {port:<10} {code:<5} {country:<25}"
+        f"{anonymity:<15} {google:<5} {https:<5} {last_checked:<10}"
+    )
+
 
 # Scrape the proxy information from a single <tr> element.
-async def scrape_proxy_info(tr) -> None:
-    # Extract all <td> elements from the <tr> element.
-    all_td: str = tr.find_all('td')
+
+
+async def scrape_proxy_info(t_tr) -> None:
+    """ Extract all <td> elements from the <tr> element."""
+    all_td: str = t_tr.find_all('td')
     if len(all_td) > 0:
         # Format the proxy information and write it to a file.
         proxy: str
-        string: str 
+        string: str
         proxy, string = format_proxy_info(all_td)
         write_to_file(proxy)
         # Print the formatted proxy information to the console.
         print(string)
 
 # Scrape the proxy information from a single page.
+
+
 async def scrape_page(url) -> None:
+    """ Send a GET request to the specified URL and retrieve the page content."""
     async with aiohttp.ClientSession() as session:
-        # Send a GET request to the specified URL and retrieve the page content.
         async with session.get(url) as response:
             page: str = await response.text()
             soup: BeautifulSoup = BeautifulSoup(page, "html.parser")
             # Find the <div> element containing the proxy table.
-            all_proxy_div: Tag | NavigableString | None = soup.find('div', class_='table-responsive')
+            all_proxy_div: Tag | NavigableString | None = soup.find(
+                'div', class_='table-responsive')
             if all_proxy_div is None:
-                raise Exception('Error: div with class "table-responsive" not found')
+                raise Exception(
+                    'Error: div with class "table-responsive" not found')
             # Find the <table> element containing the proxy information.
-            top_table: Tag | NavigableString | int | None = all_proxy_div.find('table')
+            top_table: Tag | NavigableString | int | None = all_proxy_div.find(
+                'table')
             if top_table is None:
                 raise Exception('Error: table not found')
             # Extract all <tr> elements from the <table> element.
-            all_tr:set = top_table.find_all('tr') # type: ignore
+            all_tr: set = top_table.find_all('tr')  # type: ignore
             type(all_tr)
             tasks: list = []
             # Create a task for each <tr> element and add it to the list of tasks.
-            for tr in all_tr[1:]:
-                task: Task[None] = asyncio.create_task(scrape_proxy_info(tr))
+            for p_tr in all_tr:
+                task: Task[None] = asyncio.create_task(scrape_proxy_info(p_tr))
                 tasks.append(task)
             # Wait for all tasks to complete before moving on.
             await asyncio.gather(*tasks)
 
 # Define a function to write the scraped proxy information to a file
+
+
 def write_to_file(proxy) -> None:
+    """ Write the given proxy string to a file named 'proxy.txt'."""
     with open("proxy.txt", "a") as file:
         file.write(proxy + "\n")
-        
-        
+
+
 # Define the main function that will scrape the proxy information
 if __name__ == '__main__':
     url: str = 'https://free-proxy-list.net/'
-    
+
     # Print the headers for the proxy information table
-    print("{:<20} {:<10} {:<5} {:<25} {:<15} {:<5} {:<5} {:<10}".format(
-        'IP Address', 'Port', 'Code', 'Country', 'Anonymity', 'Google', 'Https', 'Last Checked')
-    )
+    print(f"{'IP Address':<20} {'Port':<10} {'Code':<5} {'Country':<25}"
+          f"{'Anonymity':<15} {'Google':<5} {'Https':<5} {'Last Checked':<10}")
+
     # Print a separator line to make the table easier to read
-    print("------------------------------------------------------"+
+    print("------------------------------------------------------" +
           "------------------------------------------------------"
           )
     asyncio.run(scrape_page(url))
